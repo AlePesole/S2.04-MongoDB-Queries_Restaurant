3bb07c1a19dd79d166d58a229fb365456ce3913d
Alessandro Pesole <alespesole@gmail.com>
2026-03-22 01:14:55 +0100

    Update indexes.js

--- query/indexes.js
+++ query/indexes.js
@@ -1,1 +1,6 @@
 //Use this file to define indexes for your queries
+db.Restaurantes.createIndex({ name: 1 })
+db.Restaurantes.createIndex({ cuisine: 1})
+db.Restaurantes.createIndex({ borough: 1 })
+db.Restaurantes.createIndex({ "grades.score": 1 })
+db.Restaurantes.createIndex({ "location.coordinates": "2dsphere" })
