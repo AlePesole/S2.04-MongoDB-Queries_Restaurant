859daa712ca2c3d0d3753a2aa0c12f2b594f984a
Alessandro Pesole <alespesole@gmail.com>
2026-03-22 01:53:18 +0100

    Update queries.js

--- query/queries.js
+++ query/queries.js
@@ -80,7 +80,7 @@ db.restaurants.find({}, {_id: 0}).sort({name: -1});
 db.restaurants.find({}, {_id: 0}).sort({cuisine: 1, borough: -1});
 
 // 28. Mostrar direccions que no contenen el carrer.
-db.restaurants.find({$or: [{"address.street": {$exists: false}, "address.street": ""}]}, {address:1, _id:0});
+db.restaurants.find({$or: [{"address.street": {$exists: false}}, { "address.street": null }, {"address.street":""}]}, {address:1, _id:0});
 
 // 29. Seleccionar documents on el valor de `coordinate` és de tipus Double. Mostrar el name, restaurant_id i coordinades.
 db.restaurants.find({"address.coordinates":{$type:"double"}},{restaurant_id: 1, name: 1, "location.coordinates": 1, _id: 0});
